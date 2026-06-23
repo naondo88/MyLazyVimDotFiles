a01405bcbe03c92a7ae8ae2c2848cf1f3fb5e882
nick <nicholas.ao.88@gmail.com>
2026-06-23 15:55:00 -0700

    Fixing undercurl for catpuccin diagnostics, default underline which is not to my liking.

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,18 @@ return {
     "catppuccin/nvim",
     opts = {
       flavour = "mocha",
+      integrations = {
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+            hints = { "undercurl" },
+            ok = { "undercurl" },
+          },
+        },
+      },
     },
   },
   {
